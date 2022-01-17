ad25f68b5010e5077533301973c089a77bb352ba
José Neto <neto1351@gmail.com>
2022-01-17 20:25:24 -0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from "./components/Header";
 import { TransactionsTable } from "./components/TransactionsTable";
 import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 
 Modal.setAppElement('#root');
@@ -28,11 +29,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleNewTransactionModalOpen} />
       <Dashboard />
       <TransactionsTable />
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleNewTransactionModalClose}>
-        <h2>Cadastrar transação</h2>
-      </Modal>
+        onRequestClose={handleNewTransactionModalClose}
+      />
       <GlobalStyle />
     </>
   );
